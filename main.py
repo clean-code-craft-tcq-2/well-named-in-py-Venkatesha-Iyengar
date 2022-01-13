fdc03cc0a67b4ae5eeef8b068e3494a2f7e5f2f1
Venkatesha-Iyengar <90900979+Venkatesha-Iyengar@users.noreply.github.com>
2022-01-13 07:17:47 +0530

    Added Test for Reference Manual

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ if __name__ == '__main__':
   test_pair_to_number('Black', 'Orange', 12)
   test_pair_to_number('Violet', 'Slate', 25)
   test_pair_to_number('Red', 'Orange', 7)
+  test_Reference_Manual()
   print('Done :)')
